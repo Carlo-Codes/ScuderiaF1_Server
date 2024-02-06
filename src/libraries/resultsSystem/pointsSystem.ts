6e1fb9d1a5743143d8a67cbc8e3f7e831a67f511
CeeLizzle <carlo.scrocca@googlemail.com>
2024-02-06 22:58:57 +0000

    just need to specifiy fastestLap

--- src/libraries/resultsSystem/pointsSystem.ts
+++ src/libraries/resultsSystem/pointsSystem.ts
@@ -2,27 +2,30 @@ import { error } from "console";
 import { apiSportsRaceResult } from "../../model/apiSportsResponseTypes";
 import { IdriverTiers, RaceResultsStore } from "../../model/dbTypes";
 import { IdriverNameToIdMap } from "../../model/dbTypes";
+import { SelectionParameters, SelectionParamsMap } from "../../model/frontEnd";
 
 
 export class PointsSystemDistributor{
     RaceResults:RaceResultsStore|undefined;
+    driverSelectionResult:apiSportsRaceResult|undefined
     positionOverall:number|undefined;
     positionTierRelative:number|undefined;
     driverTiers : IdriverTiers|undefined
-    tier : keyof IdriverTiers|undefined;
-    driverSeleciton : number|undefined
+    tier : keyof SelectionParameters|undefined;
+    driverSeleciton : IdriverNameToIdMap|undefined
+    points:number = 0
 
 
-    init(tier:keyof IdriverTiers, raceResults:RaceResultsStore, driverId:number, driverTiers:IdriverTiers){
+    init(tier:keyof IdriverTiers, raceResults:RaceResultsStore, driver:IdriverNameToIdMap, driverTiers:IdriverTiers){
         try {
 
             const driversInTier = driverTiers[tier].drivers as IdriverNameToIdMap[]
-            let driverTierTest = driversInTier.find((d) => {d.id === driverId})
+            let driverTierTest = driversInTier.find((d) => {d.id === driver.id})
             if(driverTierTest){
                 this.RaceResults = raceResults
                 this.tier = tier
                 this.driverTiers = driverTiers
-                this.driverSeleciton = driverId
+                this.driverSeleciton = driver as IdriverNameToIdMap
             } else {
                 throw new Error('Driver Does not belong in tier')
             }
@@ -36,16 +39,19 @@ export class PointsSystemDistributor{
 
     definePositions(){
         try {
+            //dont need to make this calculation if its a dnf or fastest lap
             //overallPosition
-            const SelectionResult = this.RaceResults?.results.find((result) => {result.driver.id === this.driverSeleciton})
+            const SelectionResult = this.RaceResults?.results.find((result) => {result.driver.id === this.driverSeleciton?.id}) as apiSportsRaceResult
             if(!SelectionResult){
                 throw new Error('no overall position result')
             }
             this.positionOverall = SelectionResult?.position
+            this.driverSelectionResult = SelectionResult
             //
             //relativePosition to tier
             let driversInTiersResults:apiSportsRaceResult[] = []
-            const driversInTiers = this.driverTiers![this.tier!].drivers! as IdriverNameToIdMap[]
+            const tierName = SelectionParamsMap[this.tier!].IdriverTierName
+            const driversInTiers = this.driverTiers![tierName!].drivers! as IdriverNameToIdMap[]
             for(let i = 0; i < driversInTiers.length; i++){
                 const driverPositionResult = this.RaceResults?.results.find((result) => {result.driver.id === driversInTiers[i].id})
                 if(driverPositionResult){
@@ -65,6 +71,48 @@ export class PointsSystemDistributor{
 
     }
 
+    calculatePoints(){
+        try {
+            if(this.tier === "tier1" || this.tier === "tier2" || this.tier === "tier3"){
+                if(this.driverSelectionResult?.time === "DNF"){
+                    this.points = -5
+                } else if(this.positionTierRelative === 1){
+                    this.points = 20;
+                } else if(this.positionTierRelative === 2){
+                    this.points = 10;
+                } else if(this.points === 3){
+                    this.points = 5
+                }
+
+            } else if(this.tier === 'dnf'){
+                let DNFdriverTier:keyof IdriverTiers;
+                const possibleDriverTiers  = Object.keys(this.driverTiers!);
+                for(let i = 0; i < possibleDriverTiers.length; i++){
+                    const tierName = possibleDriverTiers[i] as keyof IdriverTiers
+                    const driversInTiers = this.driverTiers![tierName!].drivers! as IdriverNameToIdMap[]
+                    if(driversInTiers.includes(this.driverSeleciton!)){
+                        if(tierName === 'tier1'){
+                            this.points = 15
+                        }else if(tierName === 'tier2'){
+                            this.points = 10
+                        }else if (tierName === 'tier3'){
+                            this.points = 5
+                        }
+                    }
+                }
+                
+            } else if (this.tier === 'fastestLap'){
+                
+            }
+
+        } catch (error) {
+            if(error instanceof Error){
+                console.log(error)
+            }  
+        }
+    }
+
+    
 
 
 
---
+++ src/model/frontEnd.ts
@@ -0,0 +1,71 @@
+import { IdriverTiers, Team } from "./dbTypes"
+
+export interface TeamFrontEnd extends Omit<Team,'id'|'user_id'>{
+}
+
+
+
+export interface selectionParam {
+    name:keyof SelectionParameters,
+    IdriverTierName: keyof IdriverTiers | null
+    dbSelection:keyof TeamFrontEnd,
+    dbPoints:keyof TeamFrontEnd,
+    clientName:string
+}
+
+export interface SelectionParameters {
+    tier1:selectionParam,
+    tier2:selectionParam,
+    tier3:selectionParam,
+    dnf:selectionParam,
+    fastestLap:selectionParam
+}
+
+
+export const SelectionParamsMap:SelectionParameters = {  //maps selection parameters accross different serivices and data storage
+       
+    tier1:{
+        name:"tier1",
+        IdriverTierName:'tier1',
+        dbSelection:"tier1_driver_id",
+        dbPoints:"tier1_points",
+        clientName: "Tier 1",
+    }
+,
+
+    tier2:{
+        name:"tier2",
+        IdriverTierName:'tier2',
+        dbSelection:"tier2_driver_id",
+        dbPoints:"tier2_points",
+        clientName: "Tier 2"
+   }
+,
+
+    tier3:{
+        name:"tier3",
+        IdriverTierName:'tier3',
+        dbSelection:"tier3_driver_id",
+        dbPoints:"tier3_points",
+        clientName: "Tier 3"
+   }
+,
+
+    dnf:{
+        name:"dnf",
+        IdriverTierName:null,
+        dbSelection:"dnf_driver_id",
+        dbPoints:"dnf_points",
+        clientName: "DNF"
+   }
+,
+
+    fastestLap:{
+        name:'fastestLap',
+        IdriverTierName:null,
+        dbSelection:"fastest_lap_driver_id",
+        dbPoints: "fastest_lap_points",
+        clientName: "Fastest Lap"
+   }
+
+}
